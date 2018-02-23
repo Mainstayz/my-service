19a68a1d0f2776579d58641d4e08cccce865bbb7
chenlingjie <chenlingjie@cd121.com>
2018-02-23 13:25:56 +0800

    xg

--- testUtil.js
+++ testUtil.js
@@ -6,15 +6,22 @@ const moment = require('moment');
 console.log(moment('2018-02-02 00:00:00.000Z').isSame('2018-02-05 03:30:00.000Z', 'day'));
 axios({
   method: 'get',
+  // 更新估值
   // url: 'http://39.108.114.91:3002/myService/analyze/updateValuation'
   url: 'http://127.0.0.1:3002/myService/analyze/updateValuation'
+  // 更新净值
   // url: 'http://39.108.114.91:3002/myService/analyze/updateBaseInfo'
   // url: 'http://127.0.0.1:3002/myService/analyze/updateBaseInfo'
+  // 更好估值
   // url: 'http://39.108.114.91:3002/myService/analyze/betterValuation'
   // url: 'http://127.0.0.1:3002/myService/analyze/betterValuation'
+  // 添加净值
   // url: 'http://39.108.114.91:3002/myService/analyze/addRecentNetValue'
   // url: 'http://127.0.0.1:3002/myService/analyze/addRecentNetValue'
+  // 策略
   // url: 'http://127.0.0.1:3002/myService/analyze/getStrategy'
+  // 强制更新净值
+  // url: 'http://127.0.0.1:3002/myService/analyze/updateRecentNetValue'
 }).then((data) => {
   if (data.data.success) {
     console.log(data.data.data);
