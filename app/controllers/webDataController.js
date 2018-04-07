706293185677446fbb34b11aa728a0d2795bba09
xiaobxia <673806687@qq.com>
2018-04-07 21:51:10 +0800

    xg

--- app/controllers/webDataController.js
+++ app/controllers/webDataController.js
@@ -2,13 +2,21 @@
  * Created by xiaobxia on 2018/4/7.
  */
 const axios = require('axios');
+const moment = require('moment');
 exports.getWebStockdaybar = async function (ctx) {
   const query = ctx.query;
   try {
     const data = ctx.validateData({
       code: {type: 'string', required: true}
     }, query);
-    let resData = await axios.get(`https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=json&stock_code=${data.code}&step=3&start=&count=260&fq_type=no&timestamp=${Date.now()}`, {
+    let count = 0;
+    for (let i = 1; i < 36; i++) {
+      if (moment('2018-03-01').add(i * 31, 'days').isAfter(moment())) {
+        count = i * 25;
+        break;
+      }
+    }
+    let resData = await axios.get(`https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=json&stock_code=${data.code}&step=3&start=&count=${count}&fq_type=no&timestamp=${Date.now()}`, {
       headers: {
         Referer: `https://gupiao.baidu.com/stock/${data.code}.html?from=aladingpc`
       }
