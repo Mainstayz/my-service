734273edfe75b9b34221d7443f7c061f4e908c0c
xiaobxia <673806687@qq.com>
2018-07-31 14:23:32 +0800

    xg

--- app/controllers/webDataController.js
+++ app/controllers/webDataController.js
@@ -78,61 +78,78 @@ exports.getWebStockdaybar = async function (ctx) {
 };
 
 exports.getWebStockdaybarAll = async function (ctx) {
+  // const query = ctx.query;
+  // try {
+  //   const data = ctx.validateData({
+  //     code: {type: 'string', required: true},
+  //     days: {type: 'int', required: false}
+  //   }, query);
+  //   let code = '';
+  //   if (data.code.indexOf('sh') !== -1) {
+  //     code = data.code.substring(2) + 'K'
+  //   } else if (data.code.indexOf('sz') !== -1) {
+  //     code = data.code.substring(2) + 'J'
+  //   }
+  //   let resData = await axios({
+  //     method: 'get',
+  //     url: `http://v2.quotes.api.cnfol.com/chart.html?action=getStockKline&stockid=${code}&type=1&limit=${data.days || 200}&callback=jQuery1120020910699759913287_1532932371008&_=1532932371009`,
+  //   }).then((data) => {
+  //     let str = data.data.slice(data.data.indexOf('(') + 1, data.data.indexOf(')'));
+  //     let list = JSON.parse(str).List;
+  //     let listTemp = [];
+  //     for (let i = list.length - 1; i >= 0; i--) {
+  //       let item = list[i];
+  //       listTemp.push({
+  //         date: moment(new Date(item[0])).format('YYYYMMDD'),
+  //         close: item[4],
+  //         high: item[2],
+  //         low: item[3],
+  //         netChangeRatio: item[7],
+  //         open: item[1],
+  //         preClose: i === 0 ? item[1] : list[i - 1][4]
+  //       });
+  //     }
+  //     return listTemp;
+  //   });
+  //   // let resData = await axios.get(`https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=json&stock_code=${data.code}&step=3&start=&count=${count}&fq_type=no&timestamp=${Date.now()}`, {
+  //   //   headers: {
+  //   //     Referer: `https://gupiao.baidu.com/stock/${data.code}.html?from=aladingpc`
+  //   //   }
+  //   // });
+  //   let list = resData;
+  //   let listTemp = [];
+  //   for (let i = 0; i < list.length; i++) {
+  //     listTemp.push({
+  //       date: list[i].date,
+  //       kline: {
+  //         close: list[i].close,
+  //         high: list[i].high,
+  //         low: list[i].low,
+  //         netChangeRatio: list[i].netChangeRatio,
+  //         open: list[i].open,
+  //         preClose: list[i].preClose
+  //       }
+  //     })
+  //   }
+  //   ctx.body = ctx.resuccess({
+  //     list: listTemp
+  //   });
+  // } catch (err) {
+  //   ctx.body = ctx.refail(err);
+  // }
   const query = ctx.query;
   try {
     const data = ctx.validateData({
       code: {type: 'string', required: true},
       days: {type: 'int', required: false}
     }, query);
-    let code = '';
-    if (data.code.indexOf('sh') !== -1) {
-      code = data.code.substring(2) + 'K'
-    } else if (data.code.indexOf('sz') !== -1) {
-      code = data.code.substring(2) + 'J'
-    }
-    let resData = await axios({
-      method: 'get',
-      url: `http://v2.quotes.api.cnfol.com/chart.html?action=getStockKline&stockid=${code}&type=1&limit=${data.days || 200}&callback=jQuery1120020910699759913287_1532932371008&_=1532932371009`,
-    }).then((data) => {
-      let str = data.data.slice(data.data.indexOf('(') + 1, data.data.indexOf(')'));
-      let list = JSON.parse(str).List;
-      let listTemp = [];
-      for (let i = list.length - 1; i >= 0; i--) {
-        let item = list[i];
-        listTemp.push({
-          date: moment(new Date(item[0])).format('YYYYMMDD'),
-          close: item[4],
-          high: item[2],
-          low: item[3],
-          netChangeRatio: item[7],
-          open: item[1],
-          preClose: i === 0 ? item[1] : list[i - 1][4]
-        });
+    let resData = await axios.get(`https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=json&stock_code=${data.code}&step=3&start=&count=${data.days || 200}&fq_type=no&timestamp=${Date.now()}`, {
+      headers: {
+        Referer: `https://gupiao.baidu.com/stock/${data.code}.html?from=aladingpc`
       }
-      return listTemp;
     });
-    // let resData = await axios.get(`https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=json&stock_code=${data.code}&step=3&start=&count=${count}&fq_type=no&timestamp=${Date.now()}`, {
-    //   headers: {
-    //     Referer: `https://gupiao.baidu.com/stock/${data.code}.html?from=aladingpc`
-    //   }
-    // });
-    let list = resData;
-    let listTemp = [];
-    for (let i = 0; i < list.length; i++) {
-      listTemp.push({
-        date: list[i].date,
-        kline: {
-          close: list[i].close,
-          high: list[i].high,
-          low: list[i].low,
-          netChangeRatio: list[i].netChangeRatio,
-          open: list[i].open,
-          preClose: list[i].preClose
-        }
-      })
-    }
     ctx.body = ctx.resuccess({
-      list: listTemp
+      list: resData.data.mashData
     });
   } catch (err) {
     ctx.body = ctx.refail(err);
