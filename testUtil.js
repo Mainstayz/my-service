ad99c5e00b86472167c4156a77c7ac483b96807f
xiaobxia <673806687@qq.com>
2018-11-02 11:32:26 +0800

    xg

--- testUtil.js
+++ testUtil.js
@@ -1,41 +1,41 @@
 /**
  * Created by xiaobxia on 2018/1/31.
  */
-const axios = require('axios')
+//const axios = require('axios')
 // const moment = require('moment')
 // console.log(moment('2018-02-02 00:00:00.000Z').isSame('2018-02-05 03:30:00.000Z', 'day'));
-axios({
-  method: 'get'
-  // 更新估值
-  // url: 'http://127.0.0.1:3002/myService/schedule/updateRate'
-  // url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/updateValuation'
-  // 更新净值
-  // url: 'http://39.108.114.91:3002/${config.project.projectName}/analyze/updateBaseInfo'
-  // url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/updateBaseInfo'
-  // 更好估值
-  // url: 'http://39.108.114.91:3002/${config.project.projectName}/analyze/betterValuation'
-  // url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/betterValuation'
-  // 添加净值
-  // url: 'http://39.108.114.91:3002/${config.project.projectName}/analyze/addRecentNetValue'
-  // url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/addRecentNetValue'
-  // 策略
-  // url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/getStrategy'
-  // 强制更新净值
-  // url: 'http://127.0.0.1:3002/myService/schedule/updateRecentNetValue'
-  // 判断开盘
-  // url: `http://47.98.140.76:3002/myService/schedule/verifyOpening`,
-  // 回归
-  // url: 'http://127.0.0.1:3002/myServiceV2/schedule/verifyOpening',
-  // 邮件
-  // url: `http://47.98.140.76:3002/myService/test/testEmail`
-  // url: `http://47.98.140.76:3002/myService/fund/initUserFundPosition`
-}).then((data) => {
-  if (data.data.success) {
-    console.log(data.data.data)
-  } else {
-    console.log(data)
-  }
-})
+// axios({
+//   method: 'get'
+// 更新估值
+// url: 'http://127.0.0.1:3002/myService/schedule/updateRate'
+// url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/updateValuation'
+// 更新净值
+// url: 'http://39.108.114.91:3002/${config.project.projectName}/analyze/updateBaseInfo'
+// url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/updateBaseInfo'
+// 更好估值
+// url: 'http://39.108.114.91:3002/${config.project.projectName}/analyze/betterValuation'
+// url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/betterValuation'
+// 添加净值
+// url: 'http://39.108.114.91:3002/${config.project.projectName}/analyze/addRecentNetValue'
+// url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/addRecentNetValue'
+// 策略
+// url: 'http://127.0.0.1:3002/${config.project.projectName}/analyze/getStrategy'
+// 强制更新净值
+// url: 'http://127.0.0.1:3002/myService/schedule/updateRecentNetValue'
+// 判断开盘
+// url: `http://47.98.140.76:3002/myService/schedule/verifyOpening`,
+// 回归
+// url: 'http://127.0.0.1:3002/myServiceV2/schedule/verifyOpening',
+// 邮件
+// url: `http://47.98.140.76:3002/myService/test/testEmail`
+// url: `http://47.98.140.76:3002/myService/fund/initUserFundPosition`
+// }).then((data) => {
+//   if (data.data.success) {
+//     console.log(data.data.data)
+//   } else {
+//     console.log(data)
+//   }
+// })
 // axios({
 //   method: 'get',
 //   url: 'http://v2.quotes.api.cnfol.com/chart.html?action=getStockKline&stockid=000001K&type=1&limit=100&callback=jQuery1120020910699759913287_1532932371008&_=1532932371009',
@@ -80,3 +80,6 @@ axios({
 //   let str = data.data.slice(data.data.indexOf('(') + 1, data.data.indexOf(')'));
 //   console.log(JSON.parse(str).info);
 // });
+let str = '{"aaa"}'
+str = str.replace(/"/g, '\'')
+console.log(str)
