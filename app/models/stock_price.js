33156034b768f4c12f760d40909dce1a4a71bcf1
xiaobxia <673806687@qq.com>
2018-12-12 15:12:39 +0800

    xg

--- app/models/stock_price.js
+++ app/models/stock_price.js
@@ -17,6 +17,6 @@ const schema = new Schema({
     default: Date.now
   }
 })
-schema.index({ trade_date: -1 , code: 1}, { unique: true })
+schema.index({ trade_date: -1, code: 1 }, { unique: true })
 
 module.exports = mongoose.model('StockPrice', schema)
